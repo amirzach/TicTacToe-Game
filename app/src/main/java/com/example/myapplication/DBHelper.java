cb0a22b70a022768d77df0236f8d433eccbddedf
DauntLesS21 <najmuddiniqbal@gmail.com>
2024-07-08 14:21:00 +0800

    Combining SQLite Database with UI

---
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -0,0 +1,123 @@
+package com.example.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "TicTacToeDB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "users";
+    private static final String KEY_ID = "id";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASSWORD = "password";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_USERNAME + " TEXT, "
+                + KEY_EMAIL + " TEXT, "
+                + KEY_PASSWORD + " TEXT)";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addUser(String username, String email, String password) {
+        SQLiteDatabase db = null;
+        try {
+            db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(KEY_USERNAME, username);
+            values.put(KEY_EMAIL, email);
+            values.put(KEY_PASSWORD, password);
+
+            long result = db.insert(TABLE_NAME, null, values);
+            return result != -1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (db != null && db.isOpen()) {
+                db.close();
+            }
+        }
+    }
+
+    public boolean updateUser(int id, String username, String email, String password) {
+        SQLiteDatabase db = null;
+        try {
+            db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(KEY_USERNAME, username);
+            values.put(KEY_EMAIL, email);
+            values.put(KEY_PASSWORD, password);
+
+            int result = db.update(TABLE_NAME, values, KEY_ID + " = ?", new String[]{String.valueOf(id)});
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (db != null && db.isOpen()) {
+                db.close();
+            }
+        }
+    }
+
+    public boolean checkUser(String username, String email) {
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+        try {
+            db = this.getReadableDatabase();
+            cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_USERNAME + " = ? AND " + KEY_EMAIL + " = ?", new String[]{username, email});
+            return cursor.getCount() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null && db.isOpen()) {
+                db.close();
+            }
+        }
+    }
+
+    public boolean isUsernameTaken(String username) {
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+        try {
+            db = this.getReadableDatabase();
+            cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + KEY_USERNAME + " = ?", new String[]{username});
+            return cursor.getCount() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null && db.isOpen()) {
+                db.close();
+            }
+        }
+    }
+}
